21d92510353af72db90ed5a920dff5c43f908497
hendisantika <hendisantika@yahoo.co.id>
2024-06-15 06:46:48 +0700

    Add PurchaseOrderService🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/service/PurchaseOrderService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.orderservice.service;
+
+import id.my.hendisantika.orderservice.repository.PurchaseOrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-event-driven
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/15/24
+ * Time: 06:46
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class PurchaseOrderService {
+    private final PurchaseOrderRepository purchaseOrderRepository;
+}
