99b2a304ae8db286868932a9587d2b92956a0bf1
hendisantika <hendisantika@yahoo.co.id>
2024-06-15 06:29:28 +0700

    Add createUser in UserController🫸🌀✏️📗🐧🐳

--- user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
+++ user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
@@ -1,8 +1,11 @@
 package id.my.hendisantika.userservice.controller;
 
+import id.my.hendisantika.userservice.dto.UserDto;
 import id.my.hendisantika.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +25,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/create")
+    public Long createUser(@RequestBody UserDto userDto) {
+        return this.userService.createUser(userDto);
+    }
+
 }
