ea0cf7df597ed17e9933d7a39102970f2a60323b
hendisantika <hendisantika@yahoo.co.id>
2024-06-15 06:52:37 +0700

    Add OpenAPI Annotation🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
@@ -2,6 +2,11 @@ package id.my.hendisantika.orderservice.controller;
 
 import id.my.hendisantika.orderservice.entity.PurchaseOrder;
 import id.my.hendisantika.orderservice.service.PurchaseOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,16 +30,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/order-service")
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Tag(name = "Order", description = "Order API with documentation annotations")
 public class OrderController {
 
     private final PurchaseOrderService purchaseOrderService;
 
     @GetMapping("/all")
+    @Operation(
+            summary = "Get All Orders",
+            description = "Get All Orders .",
+            tags = {"Order"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Long.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public List<PurchaseOrder> getAllOrders() {
         return this.purchaseOrderService.getPurchaseOrders();
     }
 
     @PostMapping("/create")
+    @Operation(
+            summary = "Create New Order",
+            description = "Create New Order .",
+            tags = {"Order"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Long.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public void createOrder(@RequestBody PurchaseOrder purchaseOrder) {
         this.purchaseOrderService.createPurchaseOrder(purchaseOrder);
     }
