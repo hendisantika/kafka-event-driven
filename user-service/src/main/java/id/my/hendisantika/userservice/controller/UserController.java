7cce322010117a415f54ec0b3592a0a3a8a13ae3
hendisantika <hendisantika@yahoo.co.id>
2024-06-15 06:29:15 +0700

    Add UserController🫸🌀✏️📗🐧🐳

---
+++ user-service/src/main/java/id/my/hendisantika/userservice/controller/UserController.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.userservice.controller;
+
+import id.my.hendisantika.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-event-driven
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/15/24
+ * Time: 06:28
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/user-service")
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class UserController {
+
+    private final UserService userService;
+}
