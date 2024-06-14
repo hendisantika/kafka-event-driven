f4a93891a90ea2d8a86fa98f706a7c483c416f1c
hendisantika <hendisantika@yahoo.co.id>
2024-06-15 06:24:26 +0700

    Add User Entity🫸🌀✏️📗🐧🐳

---
+++ user-service/src/main/java/id/my/hendisantika/userservice/entity/User.java
@@ -0,0 +1,32 @@
+package id.my.hendisantika.userservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : kafka-event-driven
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/15/24
+ * Time: 06:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+}
